023ffdd1361e32a9240f5fc7d3b30c762a1045b3
Katarina Rafcikova <rafcikova@gmail.com>
2017-04-22 15:45:31 +0200

    Added contact page, edited footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :dictionaries
+  get '/contact', to: 'contact#contact'
+  get '/signup',  to: 'users#new'
   get 'welcome/index'
   root 'welcome#index'
 end
