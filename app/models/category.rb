3b0ac6415b9c3c6f6cd7cc312ed5846714f6e42f
Katarina Rafcikova <rafcikova@gmail.com>
2017-04-22 20:24:42 +0200

    Added validation for user name and password

---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ApplicationRecord
+  #  TODO Vadi ze to overriduje???? - SPYTAT SA NA CVIKU!
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329092703) do
+ActiveRecord::Schema.define(version: 20170422175352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,10 +75,11 @@ ActiveRecord::Schema.define(version: 20170329092703) do
   create_table "users", force: :cascade do |t|
     t.text     "name"
     t.text     "email"
-    t.text     "password"
+    t.text     "password_digest"
     t.datetime "registred_date"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
   add_foreign_key "dictionaries", "categories"
