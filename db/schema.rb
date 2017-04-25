58b78510a3ac5f578e74e31b7780e216294796c9
Katarina Rafcikova <rafcikova@gmail.com>
2017-04-25 14:25:17 +0200

    Finished user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170423205439) do
+ActiveRecord::Schema.define(version: 20170425093846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,9 +77,10 @@ ActiveRecord::Schema.define(version: 20170423205439) do
     t.text     "email"
     t.text     "password_digest"
     t.datetime "registred_date"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
