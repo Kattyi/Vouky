055de9271ad28cc9905903ec4a5c9e7a4bc188ca
Katarina Rafcikova <rafcikova@gmail.com>
2017-04-28 17:40:26 +0200

    Added search for languages

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -2,10 +2,28 @@ class LanguagesController < ApplicationController
 
   def index
     id = current_user
-    @languages = Language.joins(:dictionaries).where(dictionaries: {user_id: id}).group(:name).order("count(languages.name) DESC").count
+    if params[:term]
+      @languages = Language.joins(:dictionaries)
+                       .where(dictionaries: {user_id: id})
+                       .where('lower(name) LIKE ?', "%#{params[:term]}%".downcase)
+                       .group(:name)
+                       .order("count(languages.name) DESC").count
+    else
+      @languages = Language.joins(:dictionaries)
+                       .where(dictionaries: {user_id: id})
+                       .group(:name)
+                       .order("count(languages.name) DESC")
+                       .count
+    end
     @languages_ids = Language.joins(:dictionaries).select(:id)
+
   end
 
-  # TODO dorobit search
+  private
+
+  # whitelist the parameters to prevent wrongful mass assignment
+  def dictionary_params
+    params.require(:language).permit(:term)
+  end
 
 end
